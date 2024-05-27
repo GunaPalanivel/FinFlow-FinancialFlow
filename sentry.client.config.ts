100e672fb385150292a6264a2f443724ad964bcf
GunaPalanivel <gp5901@srmist.edu.in>
2024-05-28 03:44:52 +0530

    chore: Update npm dependencies and tailwind configuration

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://c3a7aa55db77c743af6a2a9adc1692ff@o4507330336587776.ingest.us.sentry.io/4507330339143680",
+  dsn: "https://e300b41df2ffb6d4592972400a25c0d3@o4506876178464768.ingest.us.sentry.io/4507159179034624",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
